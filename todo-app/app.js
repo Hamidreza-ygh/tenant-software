6f3f0c8a881b1f238e71e8d09b2e24d2e1dec82e
Hamidreza-ygh <hr.yaghoubi@gmail.com>
2024-05-13 16:11:35 +0200

    Keep database connection in loop

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,16 +1,31 @@
 require("dotenv").config();
+var ConnectoToDatabase = require('./app/config/connection');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var connection = require('./app/config/connection');
+// var ConnectoToDatabase = require('./app/config/connection');
+
+var connection = new ConnectoToDatabase({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  // ssl: {
+  //   ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
+  // }
+});
+
 var routes = require('./app/controllers/routes');
+// const { connect } = require("./app/config/connection");
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 connection.connect();
-connection.init();
+// ConnectoToDatabase.connect();
+// ConnectoToDatabase.init();
 routes.configure(app);
 
 var server = app.listen(8000, function(){
--- todo-app/app/config/connection.js
+++ todo-app/app/config/connection.js
@@ -1,70 +1,101 @@
+// var mysql = require('mysql2');
+// var fs = require('fs');
+
+// function Connection() {
+//   // this.pool = null;
+//   // this.connection = mysql.createConnection("mysql://avnadmin:AVNS_UzVmkirWWB6rs-e9W4N@tenant-1-exoscale-281607c5-270f-47aa-b92e-042fb1218654.f.aivencloud.com:21699/defaultdb?ssl-mode=REQUIRED");
+//   this.connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     // ssl: {
+//     //   ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
+//     // }
+//   });
+
+//   this.connect = function() {
+//     return new Promise((resolve, reject) => {
+//       this.connection.connect(function(err) {
+//         if (err) {
+//           console.error('error connecting: ' + err.stack);
+//           reject(err);
+//         } else {
+//           console.log('connected as id ');
+//           resolve();
+//         }
+//       });
+//     });
+//   };
+  
+//   this.init = function() {
+//     // Import schema.sql file into the database
+//     var schema = fs.readFileSync(__dirname + '/db/schema.sql', 'utf8');
+//     this.connect().then(() => {
+//       this.connection.query(schema, (err) => { // Use arrow function to preserve 'this' context
+//         if (err) {
+//           console.error('error importing schema: ' + err.stack);
+//           return;
+//         }
+//         console.log('schema imported successfully');
+//       });
+//     }).catch((err) => {
+//       console.error('error connecting: ' + err.stack);
+//     });
+//   };
+// }
+
+// module.exports = new Connection();
+
+
 var mysql = require('mysql2');
 var fs = require('fs');
 
-function Connection() {
+class ConnectoToDatabase {
   // this.pool = null;
   // this.connection = mysql.createConnection("mysql://avnadmin:AVNS_UzVmkirWWB6rs-e9W4N@tenant-1-exoscale-281607c5-270f-47aa-b92e-042fb1218654.f.aivencloud.com:21699/defaultdb?ssl-mode=REQUIRED");
-  this.connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    // ssl: {
-    //   ca: fs.readFileSync(__dirname + '/certs/ca.pem'),
-    // }
-  });
+  constructor(config) {
+    this.config = config;
+    console.log('config:', config);
+    // this.connection = mysql.createConnection(this.config);
+  }
 
-  this.connect = function() {
-    return new Promise((resolve, reject) => {
-      this.connection.connect(function(err) {
-        if (err) {
-          console.error('error connecting: ' + err.stack);
-          reject(err);
+  connect() {
+    this.connection = mysql.createConnection(this.config);
+
+    this.connection.connect((err) => {
+        if(err) {
+            console.log('Error connecting to Db', err);
+            console.log('Retrying in 5 seconds...');
+            setTimeout(() => this.connect(), 5000); // Retry every 5 seconds
         } else {
-          console.log('connected as id ');
-          resolve();
+            console.log('Connection established successfully');
+            this.init();
         }
-      });
     });
-  };
+
+    // connection.on('error', (err) => {
+    //     console.log('Database error', err);
+    //     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // If connection lost, retry
+    //         this.connect();
+    //     } else {
+    //         throw err;
+    //     }
+    // });
+}
   
-  this.init = function() {
+  init() {
     // Import schema.sql file into the database
     var schema = fs.readFileSync(__dirname + '/db/schema.sql', 'utf8');
-    this.connect().then(() => {
-      this.connection.query(schema, (err) => { // Use arrow function to preserve 'this' context
-        if (err) {
-          console.error('error importing schema: ' + err.stack);
-          return;
-        }
-        console.log('schema imported successfully');
-      });
-    }).catch((err) => {
-      console.error('error connecting: ' + err.stack);
+    this.connection.query(schema, (err) => { // Use arrow function to preserve 'this' context
+      if (err) {
+        console.error('error importing schema: ' + err.stack);
+        return;
+      }
+      console.log('schema imported successfully');
     });
   };
-  // this.connect = function() {
-  //   this.connection.connect(function(err) {
-  //     if (err) {
-  //       console.error('error connecting: ' + err.stack);
-  //       return;
-  //     }
-  //     console.log('connected as id ');
-  //   });
-  // };
-
-  // this.init = function() {
-  //   // Import schema.sql file into the database
-  //   var schema = fs.readFileSync(__dirname + '/db/schema.sql', 'utf8');
-  //   this.connection.query(schema, (err) => { // Use arrow function to preserve 'this' context
-  //     if (err) {
-  //       console.error('error importing schema: ' + err.stack);
-  //       return;
-  //     }
-  //     console.log('schema imported successfully');
-  //   });
-  // };
 }
 
-module.exports = new Connection();
+module.exports = ConnectoToDatabase;
