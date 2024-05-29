301b977cecc73e523f8028dcda28c9ce7a4f21d6
Hamidreza-ygh <hr.yaghoubi@gmail.com>
2024-05-29 22:42:37 +0200

    Adding Authorization Header to CORS setup

--- todo-app/app.js
+++ todo-app/app.js
@@ -25,7 +25,7 @@ var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://tenantodo.life"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific methods
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Allow specific headers
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow specific headers
   res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
   next();
 });
